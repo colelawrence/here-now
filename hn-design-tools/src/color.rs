6589b896780f9b42812dbe4e6694a5c77e513550
Cole Lawrence <cole@colelawrence.com>
2023-07-12 22:52:18 -0300

    ref: Add webpages on color

--- hn-design-tools/src/color.rs
+++ hn-design-tools/src/color.rs
@@ -1,3 +1,7 @@
+//! Overview of plans for color:
+//!
+//! Initially, my impression is we should travel across the HCT color space to choose
+//! cohesive colors and travel across the Oklab space for interpolation or gradient.
 use crate::prelude::*;
 
 pub mod input;
