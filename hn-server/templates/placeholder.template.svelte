deb61f4a794a5f29583f5546aa7214c89ded7f85
Cole Lawrence <cole@colelawrence.com>
2023-08-01 22:27:28 -0300

    dev: Add placeholder template

---
+++ hn-server/templates/placeholder.template.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Header from "./Header.svelte";
+  import {devStringify} from "./helpers/devstringify";
+</script>
+
+<Header />
+
+<div class="result">{devStringify($$restProps)}</div>
+
+<style>
+  .result {
+    padding: 1rem;
+    background: rgba(125, 255, 125, 0.2);
+    white-space: pre-wrap;
+  }
+</style>
