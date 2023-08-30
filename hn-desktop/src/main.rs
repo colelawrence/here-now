167b24741122cc658fc933e7d6218ae4f89b6ba9
Cole Lawrence <cole@colelawrence.com>
2023-08-30 13:43:58 -0300

    Add test for prework runtime measurement

--- hn-desktop/src/main.rs
+++ hn-desktop/src/main.rs
@@ -9,3 +9,17 @@ fn main() {
         ui_holder.set(ui);
     });
 }
+
+#[test]
+#[ignore]
+fn test_startup_time() {
+    let start_time = std::time::Instant::now();
+    hn_tracing::expect_init_logger("hn-desktop-startup-time");
+    let ui_holder = ui_holder_mutex::UIHolderMutex::empty();
+    let executor = hn_desktop_executor::main(Box::new(ui_holder.clone()));
+    hn_desktop_ui::main_blocking(executor, move |_| {
+        // print time elapsed since start
+        println!("startup time: {:?}", start_time.elapsed());
+        std::process::exit(0);
+    });
+}
