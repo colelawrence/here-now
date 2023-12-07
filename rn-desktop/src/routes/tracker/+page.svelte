3df4a2d8ca7499076717cb9000cff05c20cd9f3e
Cole Lawrence <cole@colelawrence.com>
2023-12-07 15:19:01 -0300

    Fix tracker styling

--- rn-desktop/src/routes/tracker/+page.svelte
+++ rn-desktop/src/routes/tracker/+page.svelte
@@ -12,43 +12,38 @@
   const nextTodo = $derived(app.todos[0]);
 </script>
 
-<div class="p-2">
-  <main
-    class="p-2 grow flex gap-2 justify-stretch border-2 border-sys-on-primary bg-sys-primary rounded-lg shadow-sm"
-    data-tauri-drag-region
-  >
-    {#key nextTodo?.id}
-      {#if nextTodo != null}
-        <div class="flex grow gap-4 items-center" data-tauri-drag-region>
-          <TodoItemCheckbox todo={nextTodo} />
-          <label for={nextTodo.htmlCheckboxId} class="text-lg font-medium pointer-events-none select-none"
-            >{nextTodo.text}</label
-          >
-        </div>
-      {:else}
-        <div class="grow text-sys-on-primary text-opacity-50" data-tauri-drag-region>All done.</div>
-      {/if}
-    {/key}
-
-    {#if app.workState.state === "working" || app.workState.state === "break"}
-      <Timer info={app.workState.timer} popoverPlacement="left-start" />
-      <button on:click={app.workState.stopSession}>
-        <Stop />
-      </button>
-      {#if app.workState.state === "break"}
-        <button on:click={app.workState.continueWorking}>
-          <Play />
-        </button>
-      {/if}
+<main class="p-2 grow flex gap-2 justify-stretch rounded-lg shadow-sm" data-tauri-drag-region>
+  {#key nextTodo?.id}
+    {#if nextTodo != null}
+      <div class="flex grow gap-4 items-center" data-tauri-drag-region>
+        <TodoItemCheckbox todo={nextTodo} />
+        <label for={nextTodo.htmlCheckboxId} class="text-lg font-medium pointer-events-none select-none"
+          >{nextTodo.text}</label
+        >
+      </div>
     {:else}
-      <button on:click={app.workState.startSession}>
+      <div class="grow text-sys-on-primary text-opacity-50" data-tauri-drag-region>All done.</div>
+    {/if}
+  {/key}
+
+  {#if app.workState.state === "working" || app.workState.state === "break"}
+    <Timer info={app.workState.timer} popoverPlacement="left-start" />
+    <button on:click={app.workState.stopSession}>
+      <Stop />
+    </button>
+    {#if app.workState.state === "break"}
+      <button on:click={app.workState.continueWorking}>
         <Play />
       </button>
     {/if}
-    {#if app.workState.state === "working"}
-      <button on:click={app.workState.expandIntoPlanner}>
-        <ArrowUp />
-      </button>
-    {/if}
-  </main>
-</div>
+  {:else}
+    <button on:click={app.workState.startSession}>
+      <Play />
+    </button>
+  {/if}
+  {#if app.workState.state === "working"}
+    <button on:click={app.workState.expandIntoPlanner}>
+      <ArrowUp />
+    </button>
+  {/if}
+</main>
